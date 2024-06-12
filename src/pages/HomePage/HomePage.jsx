63b0b68961cd423f02c0be768802da65c1e5863d
Rafay Memon <rafayrashid200611@gmail.com>
2024-06-12 09:44:20 +0500

    Home Designing Begins

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,20 @@
+import { Box, Container, Flex } from '@chakra-ui/react'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <Container maxW={"container.lg"}>
+      <Flex gap={20}>
+        <Box flex={2} py={10} border={"1px solid blue"}>
+          Feed Box
+        </Box>
+        <Box maxWidth={"300px"} flex={3} mr={20} display={{base:"none", lg:"block"}} border={"1px solid red"}>
+
+          Suggestions Box
+        </Box>
+
+
+      </Flex>
+    </Container>
   )}
 export default HomePage
